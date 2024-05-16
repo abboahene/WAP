e986520e0ba47d7abf6c2c23d924c2cedbec50c1
Alfred <alfredboahene.7@gmail.com>
2024-05-15 23:11:29 -0500

    lab 13

--- week3/Lab-13-Node/server.js
+++ week3/Lab-13-Node/server.js
@@ -7,5 +7,9 @@ app.use(express.json());
 
 app.use("/books", booksRouter);
 
+app.use((error, req, res, next) => {
+    res.status(500).json({ message: error.message });
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`server running on: ${port}`));
